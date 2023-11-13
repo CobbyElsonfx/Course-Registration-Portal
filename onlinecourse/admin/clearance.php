abe089be7f2c00fd8b676529188a956dcf908030
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-13 14:29:18 +0000

    added some new changes

--- onlinecourse/admin/clearance.php
+++ onlinecourse/admin/clearance.php
@@ -120,6 +120,5 @@ if (strlen($_SESSION['alogin']) == 0) {
         <script src="../assets/js/jquery-1.11.1.js"></script>
         <script src="../assets/js/bootstrap.js"></script>
     </body>
-
     </html>
 <?php } ?>
