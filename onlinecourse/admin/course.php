0a5ffbd60d3ab258677c37b9eab1999be8ddbbf0
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-26 14:47:46 +0000

    divided the course sections into various levels

--- onlinecourse/admin/course.php
+++ onlinecourse/admin/course.php
@@ -4,14 +4,17 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
-
-    //Code for Insertion
     if (isset($_POST['submit'])) {
         $coursecode = $_POST['coursecode'];
         $coursename = $_POST['coursename'];
         $courseunit = $_POST['courseunit'];
         $seatlimit = $_POST['seatlimit'];
-        $ret = mysqli_query($con, "insert into course(courseCode,courseName,courseUnit,noofSeats) values('$coursecode','$coursename','$courseunit','$seatlimit')");
+        $level = $_POST['level'];
+        $program = $_POST['program'];
+
+        $ret = mysqli_query($con, "INSERT INTO course(courseCode, courseName, courseUnit, noofSeats, level_id, programme_id) 
+                                   VALUES ('$coursecode', '$coursename', '$courseunit', '$seatlimit', '$level', '$program')");
+
         if ($ret) {
             echo '<script>alert("Course Created Successfully !!")</script>';
             echo '<script>window.location.href=course.php</script>';
@@ -21,9 +24,8 @@ if (strlen($_SESSION['alogin']) == 0) {
         }
     }
 
-    //Code for Insertion
     if (isset($_GET['del'])) {
-        mysqli_query($con, "delete from course where id = '" . $_GET['id'] . "'");
+        mysqli_query($con, "DELETE FROM course WHERE id = '" . $_GET['id'] . "'");
         echo '<script>alert("Course deleted!!")</script>';
         echo '<script>window.location.href=course.php</script>';
     }
@@ -48,17 +50,15 @@ if (strlen($_SESSION['alogin']) == 0) {
         <div class="studensPortalHeader">
             <h1 class="studentPortal">Course</h1>
         </div>
-        <!-- LOGO HEADER END-->
         <?php if ($_SESSION['alogin'] != "") {
             include('includes/menubar.php');
         }
         ?>
-        <!-- MENU SECTION END-->
         <div class="content-wrapper">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="page-head-line">Course </h1>
+                        <h1 class="page-head-line">Course</h1>
                     </div>
                 </div>
                 <div class="row">
@@ -72,7 +72,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                                 <?php echo htmlentities($_SESSION['msg']); ?>
                                 <?php echo htmlentities($_SESSION['msg'] = ""); ?>
                             </font>
-
 
                             <div class="panel-body">
                                 <form name="dept" method="post">
@@ -94,96 +93,133 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             placeholder="Course Unit" required />
                                     </div>
 
+                                    <div class="form-group">
+                                        <label for="level">Select Level</label>
+                                        <select class="form-control" id="level" name="level" required>
+                                            <?php
+                                            $levels = mysqli_query($con, "SELECT * FROM level");
+                                            while ($row = mysqli_fetch_assoc($levels)) {
+                                                echo "<option value='" . $row['id'] . "'>" . $row['level'] . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="program">Select Program</label>
+                                        <select class="form-control" id="program" name="program" required>
+                                            <?php
+                                            $programs = mysqli_query($con, "SELECT * FROM programme");
+                                            while ($row = mysqli_fetch_assoc($programs)) {
+                                                echo "<option value='" . $row['id'] . "'>" . $row['program'] . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="seatlimit">Seat Limit</label>
+                                        <input type="text" class="form-control" id="seatlimit" name="seatlimit"
+                                            placeholder="Seat Limit" required />
+                                    </div>
 
                                     <button type="submit" name="submit" class="btn btn-default">Submit</button>
                                 </form>
                             </div>
                         </div>
                     </div>
-
                 </div>
                 <font color="red" align="center">
                     <?php echo htmlentities($_SESSION['delmsg']); ?>
                     <?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
                 </font>
                 <div class="col-md-12">
-                    <!--    Bordered Table  -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Manage Course
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive table-bordered">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Course Code</th>
-                                            <th>Course Title </th>
-                                            <th>Creation Date</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $sql = mysqli_query($con, "select * from course");
-                                        $cnt = 1;
-                                        while ($row = mysqli_fetch_array($sql)) {
-                                            ?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Manage Course
+    </div>
+    <div class="panel-body">
+        <?php
+        // Fetch distinct levels from the database
+        $levelQuery = $con->query("SELECT DISTINCT level_id FROM course");
 
-                                            <tr>
-                                                <td>
-                                                    <?php echo $cnt; ?>
-                                                </td>
-                                                <td>
-                                                    <?php echo htmlentities($row['courseCode']); ?>
-                                                </td>
-                                                <td>
-                                                    <?php echo htmlentities($row['courseName']); ?>
-                                                </td>
+        while ($levelRow = $levelQuery->fetch_assoc()) {
+            $levelId = $levelRow['level_id'];
 
+            // Use prepared statement for fetching courses
+            $stmt = $con->prepare("SELECT * FROM course WHERE level_id = ?");
+            $stmt->bind_param("i", $levelId);
+            $stmt->execute();
 
-                                                <td>
-                                                    <?php echo htmlentities($row['creationDate']); ?>
-                                                </td>
-                                                <td>
-                                                    <a href="edit-course.php?id=<?php echo $row['id'] ?>">
-                                                        <button class="btn btn-primary"><i class="fa fa-edit "></i>
-                                                            Edit</button> </a>
-                                                    <a href="course.php?id=<?php echo $row['id'] ?>&del=delete"
-                                                        onClick="return confirm('Are you sure you want to delete?')">
-                                                        <button class="btn ">Delete</button>
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                            <?php
-                                            $cnt++;
-                                        } ?>
+            $result = $stmt->get_result();
 
-
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                    <!--  End  Bordered Table  -->
+            if ($result) {
+                // Display your data here
+        ?>
+                <div class="table-responsive table-bordered">
+                    <h3>Level <?php echo $levelId; ?></h3>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Course Code</th>
+                                <th>Course Title</th>
+                                <th>Creation Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $cnt = 1;
+                            while ($row = $result->fetch_assoc()) {
+                            ?>
+                                <tr>
+                                    <td>
+                                        <?php echo $cnt; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo htmlentities($row['courseCode']); ?>
+                                    </td>
+                                    <td>
+                                        <?php echo htmlentities($row['courseName']); ?>
+                                    </td>
+                                    <td>
+                                        <?php echo htmlentities($row['creationDate']); ?>
+                                    </td>
+                                    <td>
+                                        <a href="edit-course.php?id=<?php echo $row['id'] ?>">
+                                            <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit</button>
+                                        </a>
+                                        <a href="course.php?id=<?php echo $row['id'] ?>&del=delete" onClick="return confirm('Are you sure you want to delete?')">
+                                            <button class="btn">Delete</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php
+                                $cnt++;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
+        <?php
+                $result->free_result();
+            } else {
+                // Handle query error
+                echo "Error: " . $con->error;
+            }
+
+            $stmt->close();
+        }
+        ?>
+    </div>
+</div>
+
+
             </div>
-
-
-
-
-
         </div>
-        </div>
-        <!-- CONTENT-WRAPPER SECTION END-->
         <?php include('includes/footer.php'); ?>
-        <!-- FOOTER SECTION END-->
-        <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-        <!-- CORE JQUERY SCRIPTS -->
         <script src="../assets/js/jquery-1.11.1.js"></script>
-        <!-- BOOTSTRAP SCRIPTS  -->
         <script src="../assets/js/bootstrap.js"></script>
     </body>
 
