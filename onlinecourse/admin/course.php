49d8446e1076fbf0e80d7bfc5e7600b767a1f0ee
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-28 13:35:25 +0000

    added differentiation of courses base on programs and level

--- onlinecourse/admin/course.php
+++ onlinecourse/admin/course.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include('includes/config.php');
+
 if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
@@ -10,10 +11,11 @@ if (strlen($_SESSION['alogin']) == 0) {
         $courseunit = $_POST['courseunit'];
         $seatlimit = $_POST['seatlimit'];
         $level = $_POST['level'];
-        $program = $_POST['program'];
+        $program = isset($_POST['isCore']) ? null : $_POST['program'];
+        $isCore = isset($_POST['isCore']) ? 1 : 0;
 
-        $ret = mysqli_query($con, "INSERT INTO course(courseCode, courseName, courseUnit, noofSeats, level_id, programme_id) 
-                                   VALUES ('$coursecode', '$coursename', '$courseunit', '$seatlimit', '$level', '$program')");
+        $ret = mysqli_query($con, "INSERT INTO course(courseCode, courseName, courseUnit, noofSeats, level_id, programme_id, isCore) 
+                           VALUES ('$coursecode', '$coursename', '$courseunit', '$seatlimit', '$level', '$program', '$isCore')");
 
         if ($ret) {
             echo '<script>alert("Course Created Successfully !!")</script>';
@@ -44,6 +46,8 @@ if (strlen($_SESSION['alogin']) == 0) {
         <link href="../assets/css/bootstrap.css" rel="stylesheet" />
         <link href="../assets/css/font-awesome.css" rel="stylesheet" />
         <link href="../assets/css/style.css" rel="stylesheet" />
+        <link href="../assets/css/course.css" rel="stylesheet" />
+
     </head>
 
     <body>
@@ -86,6 +90,11 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         <input type="text" class="form-control" id="coursename" name="coursename"
                                             placeholder="Course Name" required />
                                     </div>
+                                    <div class="form-group">
+                                        <label for="isCore">Is Core Course?</label>
+                                        <input type="checkbox" id="isCore" name="isCore" value="1">
+                                    </div>
+
 
                                     <div class="form-group">
                                         <label for="courseunit">Credit Hours</label>
@@ -107,7 +116,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 
                                     <div class="form-group">
                                         <label for="program">Select Program</label>
-                                        <select class="form-control" id="program" name="program" required>
+                                        <select class="form-control" id="program" name="program" required <?php echo isset($_POST['isCore']) ? 'disabled' : ''; ?>>
                                             <?php
                                             $programs = mysqli_query($con, "SELECT * FROM programme");
                                             while ($row = mysqli_fetch_assoc($programs)) {
@@ -117,11 +126,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         </select>
                                     </div>
 
-                                    <div class="form-group">
-                                        <label for="seatlimit">Seat Limit</label>
-                                        <input type="text" class="form-control" id="seatlimit" name="seatlimit"
-                                            placeholder="Seat Limit" required />
-                                    </div>
 
                                     <button type="submit" name="submit" class="btn btn-default">Submit</button>
                                 </form>
@@ -134,94 +138,123 @@ if (strlen($_SESSION['alogin']) == 0) {
                     <?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
                 </font>
                 <div class="col-md-12">
-<div class="panel panel-default">
-    <div class="panel-heading">
-        Manage Course
-    </div>
-    <div class="panel-body">
-        <?php
-        // Fetch distinct levels from the database
-        $levelQuery = $con->query("SELECT DISTINCT level_id FROM course");
+                    <?php
+                    // Fetch distinct levels from the database
+                    $levelQuery = $con->query("SELECT DISTINCT level_id FROM course");
 
-        while ($levelRow = $levelQuery->fetch_assoc()) {
-            $levelId = $levelRow['level_id'];
+                    while ($levelRow = $levelQuery->fetch_assoc()) {
+                        $levelId = $levelRow['level_id'];
 
-            // Use prepared statement for fetching courses
-            $stmt = $con->prepare("SELECT * FROM course WHERE level_id = ?");
-            $stmt->bind_param("i", $levelId);
-            $stmt->execute();
+                        // Fetch distinct programs for this level
+                        $programQuery = $con->prepare("SELECT DISTINCT programme_id, program FROM course 
+                                                        JOIN programme ON course.programme_id = programme.id
+                                                        WHERE level_id = ?");
+                        $programQuery->bind_param("i", $levelId);
+                        $programQuery->execute();
 
-            $result = $stmt->get_result();
+                        $programResult = $programQuery->get_result();
 
-            if ($result) {
-                // Display your data here
-        ?>
-                <div class="table-responsive table-bordered">
-                    <h3>Level <?php echo $levelId; ?></h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Course Code</th>
-                                <th>Course Title</th>
-                                <th>Creation Date</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $cnt = 1;
-                            while ($row = $result->fetch_assoc()) {
-                            ?>
-                                <tr>
-                                    <td>
-                                        <?php echo $cnt; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo htmlentities($row['courseCode']); ?>
-                                    </td>
-                                    <td>
-                                        <?php echo htmlentities($row['courseName']); ?>
-                                    </td>
-                                    <td>
-                                        <?php echo htmlentities($row['creationDate']); ?>
-                                    </td>
-                                    <td>
-                                        <a href="edit-course.php?id=<?php echo $row['id'] ?>">
-                                            <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit</button>
-                                        </a>
-                                        <a href="course.php?id=<?php echo $row['id'] ?>&del=delete" onClick="return confirm('Are you sure you want to delete?')">
-                                            <button class="btn">Delete</button>
-                                        </a>
-                                    </td>
-                                </tr>
-                            <?php
-                                $cnt++;
+                        if ($programResult) {
+                            while ($programRow = $programResult->fetch_assoc()) {
+                                $programId = $programRow['programme_id'];
+
+                                // Fetch courses for this level and program
+                                $stmt = $con->prepare("SELECT * FROM course WHERE level_id = ? AND programme_id = ?");
+                                $stmt->bind_param("ii", $levelId, $programId);
+                                $stmt->execute();
+
+                                $result = $stmt->get_result();
+
+                                if ($result) {
+                                    // Display your data here
+                                    ?>
+                                    <div class="panel panel-default">
+                                        <div class="table-heading">Level
+                                            <?php echo $levelId . '00'; ?> --
+                                            <?php echo $programRow['program']; ?>
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="table-responsive table-bordered">
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>#</th>
+                                                            <th>Course Code</th>
+                                                            <th>Course Title</th>
+                                                            <th>Creation Date</th>
+                                                            <th>Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php
+                                                        $cnt = 1;
+                                                        while ($row = $result->fetch_assoc()) {
+                                                            ?>
+                                                            <tr>
+                                                                <td>
+                                                                    <?php echo $cnt; ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo htmlentities($row['courseCode']); ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo htmlentities($row['courseName']); ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo htmlentities($row['creationDate']); ?>
+                                                                </td>
+                                                                <td>
+                                                                    <a href="edit-course.php?id=<?php echo $row['id'] ?>">
+                                                                        <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit</button>
+                                                                    </a>
+                                                                    <a href="course.php?id=<?php echo $row['id'] ?>&del=delete"
+                                                                        onClick="return confirm('Are you sure you want to delete?')">
+                                                                        <button class="btn">Delete</button>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                            <?php
+                                                            $cnt++;
+                                                        }
+                                                        ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php
+                                    $result->free_result();
+                                } else {
+                                    // Handle query error
+                                    echo "Error: " . $con->error;
+                                }
                             }
-                            ?>
-                        </tbody>
-                    </table>
+                        }
+                    }
+                    ?>
                 </div>
-        <?php
-                $result->free_result();
-            } else {
-                // Handle query error
-                echo "Error: " . $con->error;
-            }
-
-            $stmt->close();
-        }
-        ?>
-    </div>
-</div>
-
-
             </div>
-        </div>
-        <?php include('includes/footer.php'); ?>
-        <script src="../assets/js/jquery-1.11.1.js"></script>
-        <script src="../assets/js/bootstrap.js"></script>
+            <script src="../assets/js/jquery-1.11.1.js"></script>
+            <script src="../assets/js/bootstrap.js"></script>
+            <script>
+                // Function to enable/disable program selection based on isCore checkbox
+                function toggleProgramSelection() {
+                    var isCoreCheckbox = document.getElementById('isCore');
+                    var programSelect = document.getElementById('program');
+
+                    programSelect.disabled = isCoreCheckbox.checked;
+                }
+
+                document.getElementById('isCore').addEventListener('change', toggleProgramSelection);
+                toggleProgramSelection();
+            </script>
+            <?php include('includes/footer.php'); ?>
+
+
     </body>
 
     </html>
-<?php } ?>
+    <?php
+}
+
+?>
